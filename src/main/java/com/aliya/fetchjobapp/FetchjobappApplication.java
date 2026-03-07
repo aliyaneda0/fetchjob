a0b34ee7d5166c898422e41f55fcb3f419abbb36
aliya neda <137129762+aliyaneda0@users.noreply.github.com>
2026-03-07 15:31:46 +0530

    First Commit

---
+++ src/main/java/com/aliya/fetchjobapp/FetchjobappApplication.java
@@ -0,0 +1,13 @@
+package com.aliya.fetchjobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchjobappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchjobappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliya/fetchjobapp/FetchjobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliya.fetchjobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchjobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
